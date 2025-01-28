5beaa1b17deb96590d091b2f14bdbda59d6a7edd
jitvarpatil <jitvar.patil@cometchat.com>
2025-01-28 13:37:16 +0000

    Generate sample app with trimmed modifications - sample-app-branch-20250128133715

--- SampleAppPushNotificationAPNs/View Controllers/LoginVC/ChangeAppCredentialsVC.swift
+++ SampleAppPushNotificationAPNs/View Controllers/LoginVC/ChangeAppCredentialsVC.swift
@@ -62,7 +62,7 @@ class ChangeAppCredentialsVC: UIViewController {
         let button = UIButton()
         button.addTarget(self, action: #selector(onRegionSelected(_ :)), for: .primaryActionTriggered)
         button.setTitle("  India", for: .normal)
-        button.setImage(UIImage(named: "india-flag"), for: .normal)
+        button.setImage(UIImage(named: "India-flag"), for: .normal)
         button.titleLabel?.font = CometChatTypography.Button.medium
         button.setTitleColor(CometChatTheme.textColorSecondary, for: .normal)
         button.layer.borderWidth = 0.8
@@ -124,6 +124,7 @@ class ChangeAppCredentialsVC: UIViewController {
     }()
     
     var selectedRegion = ""
+    var isPresented = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -238,19 +239,23 @@ class ChangeAppCredentialsVC: UIViewController {
     @objc func onContinueButtonClicked() {
         
         if checkForError() {
-            
             AppConstants.APP_ID = appIDTextFiled.textField.text ?? ""
             AppConstants.REGION = selectedRegion
             AppConstants.AUTH_KEY = authKeyTextField.textField.text ?? ""
             
-            
-            if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-               let sceneDelegate = scene.delegate as? SceneDelegate {
-                sceneDelegate.initialisationCometChatUIKit()
-            }
-            self.dismiss(animated: true)
-            
-        }
-    }
-
+            AppConstants.saveAppConstants()
+             
+             // Initialize CometChatUIKit
+             if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                let sceneDelegate = scene.delegate as? SceneDelegate {
+                 sceneDelegate.initialisationCometChatUIKit(completion: {
+                     if self.isPresented {
+                         self.dismiss(animated: true)
+                     } else {
+                         sceneDelegate.setRootViewController(UINavigationController(rootViewController: LoginWithUidVC()))
+                     }
+                 })
+             }
+         }
+     }
 }
